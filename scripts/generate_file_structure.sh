25dfce0c3f4156a27887362c04e9cd8a892948f3
Damilola Jerugba <hello@damiisdandy.com>
2024-01-31 10:05:02 +0100

    Added handly script

---
+++ scripts/generate_file_structure.sh
@@ -0,0 +1,14 @@
+# get filder name from arguements
+folder_name=$1
+
+# slugify function
+slugify () {
+    echo "$folder_name" | iconv -t ascii//TRANSLIT | sed -r s/[~\^]+//g | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z
+}
+file_name=`slugify`
+
+# make new directory
+mkdir "$folder_name"
+
+# create files
+touch "$folder_name/${file_name}.py" "$folder_name/${file_name}_test.py" "$folder_name/README.md"
