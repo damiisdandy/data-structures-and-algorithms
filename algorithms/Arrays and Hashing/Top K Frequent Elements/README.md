c308c97fbf32603fdd7885b6a5532e06352b6f9a
Damilola Jerugba <hello@damiisdandy.com>
2024-02-01 18:12:13 +0100

    Added Valid Sudoku

--- algorithms/Arrays and Hashing/Top K Frequent Elements/README.md
+++ algorithms/Arrays and Hashing/Top K Frequent Elements/README.md
@@ -12,10 +12,10 @@ Input: nums = [1], k = 1
 Output: [1]
  
 Constraints:
-- 1 <= nums.length <= 105
-- -104 <= nums[i] <= 104
-- k is in the range [1, the number of unique elements in the array].
-- It is guaranteed that the answer is unique.
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
 ```
 
 Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
---
+++ algorithms/Arrays and Hashing/Valid Sudoku/valid_sudoku.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+
+def isValidSudoku(board):
+    """
+    :type board: List[List[str]]
+    :rtype: bool
+    """
+    row_hash = defaultdict(set)
+    column_hash = defaultdict(set)
+    square_hash = defaultdict(set)
+
+    for r in range(9):
+        for c in range(9):
+            # to group each mini 3 x 3 square e.g
+            # for cells
+            # 0,2 => (0, 0)
+            # 0,5 => (0, 1)
+            # 7,8 => (2, 2)
+            # you will notice that they appear on those index positions for the subdivided board
+            mini_square_id = (r // 3, c // 3)
+            cell = board[r][c]
+            if cell == ".":
+                continue
+            if (
+                cell in row_hash[r]
+                or cell in column_hash[c]
+                or cell in square_hash[mini_square_id]
+            ):
+                return False
+            else:
+                row_hash[r].add(cell)
+                column_hash[c].add(cell)
+                square_hash[mini_square_id].add(cell)
+    return True
