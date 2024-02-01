3e2b45f2fb8de1d19aa3adff128143f9b5822c8e
Damilola Jerugba <hello@damiisdandy.com>
2024-02-01 16:55:54 +0100

    Added Group Anagrams

---
+++ algorithms/Arrays and Hashing/Group Anagrams/group_anagrams.py
@@ -0,0 +1,16 @@
+def groupAnagrams(strs):
+    """
+    :type strs: List[str]
+    :rtype: List[List[str]]
+    """
+    hashmap = {}
+    for s in strs:
+        # sort word (all anagrams will look the same after sorting)
+        s_sorted = "".join(sorted(s))
+        if hashmap.get(s_sorted):
+            hashmap[s_sorted].append(s)
+        else:
+            hashmap[s_sorted] = [s]
+
+    result = [k for k in hashmap.values()]
+    return result
---
+++ algorithms/Arrays and Hashing/Group Anagrams/group_anagrams_test.py
@@ -0,0 +1,28 @@
+import unittest
+from .group_anagrams import groupAnagrams
+
+
+class TestArray(unittest.TestCase):
+    def test_case1(self):
+        self.assertEqual(
+            groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]),
+            [
+                [
+                    "eat",
+                    "tea",
+                    "ate",
+                ],
+                ["tan", "nat"],
+                ["bat"],
+            ],
+        )
+
+    def test_case2(self):
+        self.assertEqual(groupAnagrams([""]), [[""]])
+
+    def test_case3(self):
+        self.assertEqual(groupAnagrams(["a"]), [["a"]])
+
+
+if __name__ == "__main__":
+    unittest.main()
